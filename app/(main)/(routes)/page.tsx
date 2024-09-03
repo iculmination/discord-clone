e34a4f8b26dfdb08559af509e7e0d5016ff05477
iculmination <ovsiichuk.bohdan@gmail.com>
2024-09-03 20:26:36 +0300

    prisma + xata.io db + db.ts + mode-toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,16 @@
-const page = () => {
-  return <div>page</div>;
-};
+import { ModeToggle } from "@/components/mode-toggle";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-export default page;
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <ModeToggle />
+    </div>
+  );
+}
