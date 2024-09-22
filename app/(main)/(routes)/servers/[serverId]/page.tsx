645d9bd738b8f1868cf1a588dc687fb68b911d26
iculmination <ovsiichuk.bohdan@gmail.com>
2024-09-22 22:59:40 +0300

    prisma models + mobile navbar + channel header

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,41 @@
-const ServerIdPage = () => {
-  return <div>server id page</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: { some: { profileId: profile.id } },
+    },
+    include: {
+      channels: { where: { name: "general" }, orderBy: { createdAt: "asc" } },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return (
+      <div className="text-rose-500 font-semibold">
+        Error: General channel missing
+      </div>
+    );
+  }
+
+  return redirect(`/servers/${params?.serverId}/channels/${initialChannel.id}`);
 };
 
 export default ServerIdPage;
